33b60483d35a119589ed04825d59c28efd98c62a
Daniel <129331368+ilhs2014350@users.noreply.github.com>
2024-09-19 17:57:19 +0000

    in progress

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+  // all code in Java sits inside a class
+  
+  // every java program needs a java main method
+  // for the next while, ALL code you write will live inside main 
+  public static void main(String[] args) {
+  System.out.println("Hello World");
+  System.out.println("");
+  System.out.println("suffering");  
+  }
+    
+}
