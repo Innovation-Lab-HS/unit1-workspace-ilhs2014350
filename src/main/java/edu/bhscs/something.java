bd789212c77147dea534108d075b834121103e18
Daniel <129331368+ilhs2014350@users.noreply.github.com>
2024-09-19 17:57:43 +0000

    in progress

---
+++ src/main/java/edu/bhscs/something.java
@@ -0,0 +1,8 @@
+class something {
+  public static void main(String[] args) {
+    // reminder all code rewrite goes here in main
+    System.out.println("howdy");
+    System.out.println("amongus");
+    System.out.println("\"hello\"");
+  }
+}
